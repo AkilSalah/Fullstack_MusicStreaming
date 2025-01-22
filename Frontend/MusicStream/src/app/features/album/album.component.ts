fd40d89a5788c12029ae22df7bd5da62aea7769c
salah <akilsalaheddine203@gmail.com>
2025-01-22 12:58:43 +0100

    display albums

--- Frontend/MusicStream/src/app/features/album/album.component.ts
+++ Frontend/MusicStream/src/app/features/album/album.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlbumService } from '../../core/services/album.service';
+import { Album } from '../../core/models/album';
 
 @Component({
   selector: 'app-album',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './album.component.css'
 })
 export class AlbumComponent {
+  albums: Album[] = [];
+  currentPage = 0;
+  totalPages = 0;
 
+  constructor(private albumService: AlbumService) {}
+
+  ngOnInit(): void {
+    this.loadAlbums();
+  }
+  loadAlbums(): void {
+    this.albumService.getAlbums(this.currentPage).subscribe({
+      next: (data) => {
+        this.albums = data.content; 
+        this.totalPages = data.totalPages;
+      },
+      error: (err) => {
+        console.error('Erreur lors du chargement des albums :', err);
+      },
+    });
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages - 1) {
+      this.currentPage++;
+      this.loadAlbums();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 0) {
+      this.currentPage--;
+      this.loadAlbums();
+    }
+  }
 }
